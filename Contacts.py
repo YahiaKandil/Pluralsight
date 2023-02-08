70431f68442f3ad33490adf5e66069bf343d97b3
YahiaKandil <98618951+YahiaKandil@users.noreply.github.com>
2023-02-07 20:55:48 -0500

    Initial commit
    Print the list of emails of student using dictionaries and lists

---
+++ Contacts.py
@@ -0,0 +1,15 @@
+contacts = {
+    "Number": 4,
+    "Students":[
+        {"Name":"Yahia", "Email":"yahiakandil@outlook.com"},
+        {"Name":"Ali", "Email":"ali@outlook.com"},
+        {"Name":"Nick", "Email":"nick@outlook.com"},
+        {"Name":"James", "Email":"James@outlook.com"},
+        {"Name":"Kevin", "Email":"Kevin@outlook.com"}
+    ]
+}
+
+# Print the emails of students
+
+for student in contacts["Students"]:
+    print("\n" + student["Email"])
