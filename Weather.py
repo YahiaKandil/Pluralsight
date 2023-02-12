e1a2455598b79bc21334f7e77d2645974daf0e9b
YahiaKandil <98618951+YahiaKandil@users.noreply.github.com>
2023-02-11 20:09:53 -0500

    Input the city name, return the weather details
    Used functions

--- Weather.py
+++ Weather.py
@@ -1,21 +1,22 @@
 import requests
 
-api_key = "67da29cb91129f1a68c1c06c1be92daa"
-city = input("\nInput the city name: \n\n")
-#str(input("\nEnter the city name:\n\n"))
-url = "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&appid=" + api_key + "&units=imperial"
+def weather_details():
+    api_key = "67da29cb91129f1a68c1c06c1be92daa"
+    city = input("\nInput the city name: \n\n")
+    url = "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&appid=" + api_key + "&units=imperial"
+    request = requests.get(url)
+    json = request.json()
+    description = json.get("weather")[0].get("description")
+    temp_min = json.get("main").get("temp_min")
+    temp_max = json.get("main").get("temp_max")
 
-request = requests.get(url)
-json = request.json()
+    return {"description":description, 
+    "temp_min":temp_min,
+     "temp_max":temp_max}
 
-#print("\n")
-description = json.get("weather")[0].get("description")
-print("\nToday's forcast is", description, "\n")
-#print("\n")
 
-temp_min = json.get("main").get("temp_min")
-print("The minimum temperature is" , temp_min, "\n")
-
-temp_max = json.get("main").get("temp_max")
-print("The maximum temperature is" , temp_max, "\n")
+weather_det = weather_details()
+print("\nToday's forcast is", weather_det.get("description"), "\n")
+print("The minimum temperature is" , weather_det.get("temp_min"), "\n")
+print("The maximum temperature is" , weather_det.get("temp_max"), "\n")
 
